36f6b626637246c6bbbedd4304385126feb7ebe0
skrusche63 <skrusche63@googlemail.com>
2014-12-05 16:57:31 +0100

    Functionality moved to 'spark-core'.

--- src/main/scala/de/kp/spark/cluster/actor/ClusterTracker.scala
+++ src/main/scala/de/kp/spark/cluster/actor/ClusterTracker.scala
@@ -28,33 +28,6 @@ import scala.collection.mutable.HashMap
 
 class ClusterTracker extends BaseTracker(Configuration) {
   
-  override def prepareFeature(params:Map[String,String]):java.util.Map[String,Object] = {
-    
-    val now = new java.util.Date()
-    val source = HashMap.empty[String,String]
-    
-    source += Names.SITE_FIELD -> params(Names.SITE_FIELD)
-    source += Names.TIMESTAMP_FIELD -> now.getTime().toString    
- 
-    /* 
-     * Restrict parameters to those that are relevant to feature description;
-     * note, that we use a flat JSON data structure for simplicity and distinguish
-     * field semantics by different prefixes 
-     */
-    val records = params.filter(kv => kv._1.startsWith("lbl.") || kv._1.startsWith("fea."))
-    for (rec <- records) {
-      
-      val (k,v) = rec
-        
-      val name = k.replace("lbl.","").replace("fea.","")
-      source += k -> v      
-      
-    }
-
-    source
-    
-  }
-
   override def prepareSequence(params:Map[String,String]):java.util.Map[String,Object] = {
     
     val source = HashMap.empty[String,String]
